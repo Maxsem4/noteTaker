8c37f41776c67340856f39428e61b87a097fda94
Maxsem <max.semegen@gmail.com>
2020-04-26 13:32:25 -0400

    add basic routes

--- server.js
+++ server.js
@@ -3,3 +3,31 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+
+// Route that sends the user to the index page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// Route that sends the user to the notes page
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Route that sends the user to the db.json file
+app.get("/api/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "db/db.json"));
+});
